74be77552c2e5e4633232e530f955c435ac9c3b2
Sabri <126477972+sabriota@users.noreply.github.com>
2025-04-28 20:14:46 +0200

    lokal_Image
    tried to use an webcam to create randomnumbers - problem: CORS

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 let password1 = document.getElementById("pw1-el");
 let password2 = document.getElementById("pw2-el");
 let buttonGen = document.getElementById("generate-el");
+const img = document.getElementById('webcam-img');
+const canvas = document.getElementById('capture-canvas');
+const ctx = canvas.getContext('2d');
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
 
+img.onload = function() {
+    console.log(generatePW()); 
+    
+    buttonGen.addEventListener("click", function() {
+        let pw1 = generatePW();
+        let pw2 = generatePW();
+        password1.textContent = pw1;
+        password2.textContent = pw2;
+    });
+};
 
-buttonGen.addEventListener("click", function() {
-    let pw1 = generatePW ();
-    let pw2 = generatePW ();
-    password1.textContent = pw1;
-    password2.textContent = pw2;
-});
+function extractRandomness() {
+    const img = document.getElementById('webcam-img');
+    const canvas = document.getElementById('capture-canvas');
+    const ctx = canvas.getContext('2d');
 
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+
+    let randomX = Math.floor(Math.random() * img.width);
+    let randomY = Math.floor(Math.random() * img.height);
+    let pixelData = ctx.getImageData(randomX, randomY, 1, 1).data;
+
+    let randomness = (pixelData[0] + pixelData[1] + pixelData[2]) % 256;
+    return randomness; 
+}
 
 function generatePW (){
-    let password = ""
-     for(let i = 0; i < 16;i++) {
-        let randomNumber = Math.floor(Math.random()*characters.length)
-        password +=characters[randomNumber];
-      
-     }
-     return password
+    let password = "";
+    let randomness = extractRandomness(); // einmal am Anfang holen
+
+    for (let i = 0; i < 16; i++) {
+        let randomNumber = (Math.floor(Math.random() * characters.length) + randomness) % characters.length;
+        password += characters[randomNumber];
+
+
+    }
+    return password
 }
 
 console.log(generatePW())
